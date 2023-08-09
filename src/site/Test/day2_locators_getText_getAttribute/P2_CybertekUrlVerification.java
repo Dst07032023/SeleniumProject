7f3e2476646b36aa589b870ae380ca2d4945671a
Dst07032023 <bovari.deonisis@gmail.com>
2023-08-09 14:56:13 +0200

    day 2 practice 2

---
+++ src/site/Test/day2_locators_getText_getAttribute/P2_CybertekUrlVerification.java
@@ -0,0 +1,15 @@
+package src.site.Test.day2_locators_getText_getAttribute;
+
+public class P2_CybertekUrlVerification {
+
+    public static void main(String[] args) {
+
+        //TC #2: Cybertek practice tool verifications
+        //1. Open Chrome browser
+        //2. Go to https://practice cybertekschool.com
+        //3. Verify URL contains
+        //Expected: cybertekschool
+        //4. Verifytitle:
+        //Expected: Practice
+    }
+}
