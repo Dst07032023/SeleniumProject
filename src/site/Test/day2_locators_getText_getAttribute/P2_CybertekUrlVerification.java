0880fd28de29e0cbbe54fe20fe0f369176ea5f66
Dst07032023 <bovari.deonisis@gmail.com>
2023-08-09 15:15:03 +0200

    day 2 practice 2

--- src/site/Test/day2_locators_getText_getAttribute/P2_CybertekUrlVerification.java
+++ src/site/Test/day2_locators_getText_getAttribute/P2_CybertekUrlVerification.java
@@ -1,15 +1,39 @@
 package src.site.Test.day2_locators_getText_getAttribute;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class P2_CybertekUrlVerification {
 
     public static void main(String[] args) {
 
         //TC #2: Cybertek practice tool verifications
+
+        //SETUP THE BROWSER DRIVER
+        WebDriverManager.chromedriver().setup();
+
         //1. Open Chrome browser
+        WebDriver driver = new ChromeDriver();
+
+        //maximize the page
+        driver.manage().window().maximize();
+
         //2. Go to https://practice cybertekschool.com
+        driver.get("http://practice.cybertekschool.com/");
+
         //3. Verify URL contains
         //Expected: cybertekschool
-        //4. Verifytitle:
+        String expectedInUrl = "cybertekschool";
+        String actualUrl = driver.getCurrentUrl();
+
+        if(actualUrl.contains(expectedInUrl)){
+            System.out.println("Url verification PASSED!");
+        }else{
+            System.out.println("Url verification FAILED!!!");
+        }
+
+        //4. Verify title:
         //Expected: Practice
     }
 }
