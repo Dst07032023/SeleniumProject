bf7185775b4e94324d12b48e588982bb1ab3cfaf
Dst07032023 <bovari.deonisis@gmail.com>
2023-08-10 16:32:04 +0200

    day 2 practice 3 click to link

--- src/site/Test/day2_locators_getText_getAttribute/P3_NavigationAndTitleVerification.java
+++ src/site/Test/day2_locators_getText_getAttribute/P3_NavigationAndTitleVerification.java
@@ -1,25 +1,57 @@
 package src.site.Test.day2_locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class P3_NavigationAndTitleVerification {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         //TC #3: Back and forth navigation
         //1- Open a chrome browser
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        driver.manage();
+        driver.manage().window().maximize();
+
         //2- Go to: https://google.com
+        driver.get("https://www.google.com");
+//        WebElement dropAgree = driver.findElement(By.cssSelector("[naam='alles accepteren']"));
+//        dropAgree.click();
+
+//        Thread.sleep(5000);
+
+
+
         //3- Click to Gmail from top right.
+        //<---locating the web element -------><-click->
+        driver.findElement(By.linkText("Gmail")).click();
+
         //4- Verify title contains:
         //Expected: Gmail
+        String expectedInTitle = "Gmail";
+        String actualTitle = driver.getTitle();
+
+        if(actualTitle.contains(expectedInTitle)){
+            System.out.println("Title verification PASSED!");
+        }else{
+            System.out.println("Title verification FAILED!");
+        }
+
         //5- Go back to Google by using the back();
+        driver.navigate().back();
+
         //6- Verify title equals:
         //Expected: Google
+        String expectedGoogleTitle = "Google";
+        String actualGoogleTitle = driver.getTitle();
+
+        if(actualGoogleTitle.equals(expectedGoogleTitle)){
+            System.out.println("Title verification PASSED!");
+        }else{
+        System.out.println("Title verification FAILED!!!");
+        }
 
     }
 }
