009110f91f2376e7d638ab4c0539045be7538512
Dst07032023 <bovari.deonisis@gmail.com>
2023-08-11 12:05:08 +0200

    day 2 practice 9 ZeroBank verifications

--- src/site/Test/day2_locators_getText_getAttribute/T2_ZeroBankAttributeVerification.java
+++ src/site/Test/day2_locators_getText_getAttribute/T2_ZeroBankAttributeVerification.java
@@ -1,12 +1,22 @@
 package src.site.Test.day2_locators_getText_getAttribute;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class T2_ZeroBankAttributeVerification {
 
     public static void main(String[] args) {
 
         //TC #2: Zero Bank link text verification
         //1. Open Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
         //2. Go to http://zero.webappsecurity.com/login.html
+        driver.get("http://zero.webappsecurity.com/login.html");
+
         //3. Verify link text from top left:
         //expected: "Zero Bank"
         //4. Verify link here attribute value contains:
