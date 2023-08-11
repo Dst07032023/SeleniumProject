2c90715eafcd1ba174d36b2855a7ec7ac328f50e
Dst07032023 <bovari.deonisis@gmail.com>
2023-08-11 12:02:16 +0200

    day 2 practice 9 ZeroBank verifications

---
+++ src/site/Test/day2_locators_getText_getAttribute/T2_ZeroBankAttributeVerification.java
@@ -0,0 +1,16 @@
+package src.site.Test.day2_locators_getText_getAttribute;
+
+public class T2_ZeroBankAttributeVerification {
+
+    public static void main(String[] args) {
+
+        //TC #2: Zero Bank link text verification
+        //1. Open Chrome browser
+        //2. Go to http://zero.webappsecurity.com/login.html
+        //3. Verify link text from top left:
+        //expected: "Zero Bank"
+        //4. Verify link here attribute value contains:
+        //Expected: "index.html"
+
+    }
+}
