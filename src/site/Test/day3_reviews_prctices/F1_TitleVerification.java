7d4ce6dc88833db8c944fbb2a931ed16e055e459
Dst07032023 <bovari.deonisis@gmail.com>
2023-08-18 18:05:04 +0200

    day 3 Facebook task 1

--- src/site/Test/day3_reviews_prctices/F1_TitleVerification.java
+++ src/site/Test/day3_reviews_prctices/F1_TitleVerification.java
@@ -1,11 +1,13 @@
 package src.site.Test.day3_reviews_prctices;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class F1_TitleVerification {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         //TC #1: Facebook title verification
         //1. Open Chrome browser
@@ -19,7 +21,25 @@ public class F1_TitleVerification {
         driver.manage().window().maximize();
 
         //2. Go to https://www.facebook.com
+        driver.get("https://www.facebook.com");
+
+        WebElement dropAgree = driver.findElement(By.xpath ("//*[.='Allow all cookies']"));
+        dropAgree.click();
+
+        Thread.sleep(3000);
+
         //3. Verify title:
         //Expected: "Facebook - Log In or Sign up"
+        String expectedTitle = "Facebook - log in or sign up";
+
+        String actualTitle = driver.getTitle();
+
+        if(actualTitle.equals(expectedTitle)){
+            System.out.println("Title verification passed!");
+        }else{
+            System.out.println("Title verification failed!!!");
+
+        }
+        driver.close();
     }
 }
---
+++ src/site/Test/day3_reviews_prctices/F2_IncorrectLoginTitleVerification.java
@@ -0,0 +1,4 @@
+package src.site.Test.day3_reviews_prctices;
+
+public class F2_IncorrectLoginTitleVerification {
+}
