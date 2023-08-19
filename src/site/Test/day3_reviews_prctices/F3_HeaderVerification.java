e4f8f9e7ca4b4a82e095694972480c3ca9df4b27
Dst07032023 <bovari.deonisis@gmail.com>
2023-08-19 12:03:59 +0200

    day 3 Facebook task 3

--- src/site/Test/day3_reviews_prctices/F3_HeaderVerification.java
+++ src/site/Test/day3_reviews_prctices/F3_HeaderVerification.java
@@ -1,21 +1,36 @@
 package src.site.Test.day3_reviews_prctices;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class F3_HeaderVerification {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         //TC #3: Facebook header verification
         //1. Open Chrome browser
-        WebDriverManager.chromedriver();
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
         //2. Go to https://www.facebook.com
         driver.get("https://www.facebook.com");
 
+        WebElement dropAgree = driver.findElement(By.xpath ("//*[.='Allow all cookies']"));
+        dropAgree.click();
+
+        Thread.sleep(3000);
+
         //3. Verify header text is as expected:
+        WebElement header = driver.findElement(By.className("_8eso"));
+
+        //short cut for auto-generating variable type
+        // alt + ENTER --> WINDOWS
+        // option + ENTER --> MAC
+
         //Expected: “Connect with friends and the world around you on Facebook.”
+        String expectedHeader = "Connect with friends and the world around you on Facebook.";
+        String actualHeader = header.getText();
     }
 }
