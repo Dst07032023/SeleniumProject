677946e20b3c7bbca549532fd5556b6cfb97329a
Dst07032023 <bovari.deonisis@gmail.com>
2023-08-19 12:23:12 +0200

    day 3 Facebook task 4

--- src/site/Test/day3_reviews_prctices/F4_AttributeValueVerification.java
+++ src/site/Test/day3_reviews_prctices/F4_AttributeValueVerification.java
@@ -1,4 +1,28 @@
 package src.site.Test.day3_reviews_prctices;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class F4_AttributeValueVerification {
+    public static void main(String[] args) throws InterruptedException {
+        //TC #4: Facebook header verification
+        //1. Open Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //2. Go to https://www.facebook.com
+        driver.get("https://www.facebook.com");
+
+        WebElement dropAgree = driver.findElement(By.xpath ("//*[.='Allow all cookies']"));
+        dropAgree.click();
+
+        Thread.sleep(3000);
+
+        //3. Verify “Create a page” link href value contains text:
+        //Expected: “registration_form”
+    }
 }
