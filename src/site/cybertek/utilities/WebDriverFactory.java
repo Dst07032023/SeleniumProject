7e3f5b42b9dbb65c79eb89881b9f12f790c3d908
Dst07032023 <bovari.deonisis@gmail.com>
2023-08-24 14:42:12 +0200

    day 4 UTILITY CLASS

---
+++ src/site/cybertek/utilities/WebDriverFactory.java
@@ -0,0 +1,13 @@
+package src.site.cybertek.utilities;
+
+public class WebDriverFactory {
+
+    // TASK: NEW METHOD CREATION
+    // Method name: getDriver
+    //Static method
+    // Accepts String arg: browserType
+    // - This arg will determine what type of browser is opened
+    // if "chrome" passed --> it will open chrome browser
+    // if "firefox" passed --> it will open firefox browser
+    // RETURN TYPE: "WebDriver"
+}
